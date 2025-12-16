08b29b1f1b2e832878fb1929ba16733153a4e772
Amairi <selsabilamairi@gmail.com>
2025-12-16 14:55:38 +0100

    Activité 14 correction

--- resources/views/components/article.blade.php
+++ resources/views/components/article.blade.php
@@ -1,8 +1,4 @@
-@php
-    use Illuminate\Support\Str;
-@endphp
-
 <article class="article">
     <h3>{{ $title }}</h3>
-    <p>{{ Str::limit($description, 30, '...') }}</p>
+    <p>{{ \Illuminate\Support\Str::limit($description, 30, '...') }}</p>
 </article>
